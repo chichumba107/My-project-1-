19836e43f07776072f08824630ffa4922ff04108
chichumba107 <159608579+chichumba107@users.noreply.github.com>
2024-03-10 14:18:55 +0300

    10.03.24

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float speed;
 
+    public Animator animator;
+
     private Vector3 _moveVector;
     private float _fallVelocity = 0;
 
@@ -23,27 +25,35 @@ public class PlayerController : MonoBehaviour
     {
         // Movement
         _moveVector = Vector3.zero;
+        var runDirection = 0; 
+
         if (Input.GetKey(KeyCode.W))
         {
             _moveVector += transform.forward;
+            runDirection = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
             _moveVector -= transform.forward;
+            runDirection = 2;
         }
         if (Input.GetKey(KeyCode.D))
         {
             _moveVector += transform.right;
+            runDirection = 3;
         }
         if (Input.GetKey(KeyCode.A))
         {
             _moveVector -= transform.right;
+            runDirection = 4;
         }
         // Jump
         if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
         {
             _fallVelocity = -jumpForce;
         }
+
+        animator.SetInteger("run direction", runDirection);
     }
 
     
